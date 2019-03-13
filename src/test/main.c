f19f0d2cd022e10ae9949c5279d8e36b642cf530
Ankush Swarnakar <ankushswar1@gmail.com>
2019-03-12 21:18:19 -0700

    edited test

--- src/test/main.c
+++ src/test/main.c
@@ -9,7 +9,10 @@ void main(void) {
 
     valves_init();
 
-    printf("Testing ")
+    printf("Testing on and off on gpio4\n");
+    test_on_off();
+
+    printf("testing all 4 valves simultaneously\n");
     turn_on_valves(100, 100, 100, 100);
     //stepper_init(GPIO_PIN19, GPIO_PIN16, GPIO_PIN20, GPIO_PIN21);
     //stepper_test();
