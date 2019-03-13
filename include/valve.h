b1a2011ca229debac03a5547a17d37c1dc92ed81
Jainil Sutaria <jsutaria@stanford.edu>
2019-03-13 16:58:05 -0700

    Fix valve header, code

--- include/valve.h
+++ include/valve.h
@@ -1,6 +1,11 @@
 #ifndef VALVE_H
 #define VALVE_H
 
+#define VALVE_1 GPIO_PIN4
+#define VALVE_2 GPIO_PIN5
+#define VALVE_3 GPIO_PIN6
+#define VALVE_4 GPIO_PIN7
+
 #include "storage.h"
 
 /*
@@ -13,8 +18,6 @@ void test_on_off();
 /*
  * Turns on all valves for a given time according to concentration
  */
- void turn_on_valves();
-
-
+void turn_on_valves(int time_1, int time_2, int time_3, int time_4);
 
 #endif
--- src/common/valve.c
+++ src/common/valve.c
@@ -8,35 +8,40 @@
 
 #define TIME_TOTAL 8000
 #define TIME_DELAY 50
-#define VALVE_1 GPIO_PIN4
-#define VALVE_2 GPIO_PIN5
-#define VALVE_3 GPIO_PIN6
-#define VALVE_4 GPIO_PIN7
 
-void valves_init() {
-  for (int i = 0; i < 4; i++) {
-    gpio_set_output(VALVE_1 + i);
-  }
+static unsigned int valves[4];
+
+void valves_init(unsigned int pin_1, unsigned int pin_2, unsigned int pin_3, unsigned int pin_4) {
+    valves[0] = pin_1;
+    valves[1] = pin_2;
+    valves[2] = pin_3;
+    valves[3] = pin_4;
+
+    gpio_set_output(pin_1);
+    gpio_set_output(pin_2);
+    gpio_set_output(pin_3);
+    gpio_set_output(pin_4);
 }
 
 void test_on_off() {
   while (1) {
-    gpio_write(VALVE_1, 1);
-    gpio_write(VALVE_2, 1);
-    gpio_write(VALVE_3, 1);
-    gpio_write(VALVE_4, 1);
+
+    for (int i = 0; i < 4; i++) {
+      gpio_write(valves[i], 1);
+    }
+
     timer_delay(1);
-    gpio_write(VALVE_1, 0);
-    gpio_write(VALVE_2, 0);
-    gpio_write(VALVE_3, 0);
-    gpio_write(VALVE_4, 0);
+
+    for (int i = 0; i < 4; i++) {
+      gpio_write(valves[i], 0);
+    }
     timer_delay(1);
   }
 }
 
 void turn_on_valves(int time_1, int time_2, int time_3, int time_4) {
   for (int i = 0; i < 4; i++) {
-    gpio_write(VALVE_1 + i, 1);
+    gpio_write(valves[i], 1);
   }
 
   // int time_1 = ingredients[0];
@@ -56,19 +61,19 @@ void turn_on_valves(int time_1, int time_2, int time_3, int time_4) {
     printf("Time 4: %d\n", time_4);
 
     if (time_1 == 1) {
-      gpio_write(VALVE_1, 0);
+      gpio_write(valves[0], 0);
     }
 
     if (time_2 == 1) {
-      gpio_write(VALVE_2, 0);
+      gpio_write(valves[1], 0);
     }
 
     if (time_3 == 1) {
-      gpio_write(VALVE_3, 0);
+      gpio_write(valves[2], 0);
     }
 
     if (time_4 == 1) {
-      gpio_write(VALVE_4, 0);
+      gpio_write(valves[3], 0);
     }
 
     timer_delay_ms(TIME_DELAY);
