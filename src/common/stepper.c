8cad1d9b13b158c15b2c798cab567c9f9e633bfc
Jainil Sutaria <jsutaria@stanford.edu>
2019-03-12 21:23:15 -0700

    Add stepper changes

--- src/common/stepper.c
+++ src/common/stepper.c
@@ -1,4 +1,6 @@
 #include "stepper.h"
+#include "gpio.h"
+#include "timer.h"
 
 static unsigned int pin_1, pin_2, pin_3, pin_4;
 
@@ -7,8 +9,18 @@ void stepper_init(unsigned int pin1, unsigned int pin2, unsigned int pin3, unsig
     pin_2 = pin2;
     pin_3 = pin3;
     pin_4 = pin4;
+    gpio_set_output(pin1);
+    gpio_set_output(pin2);
+    gpio_set_output(pin3);
+    gpio_set_output(pin4);
 }
 
 void stepper_test(void) {
+    while(1) {
+        gpio_write(pin_1, 1);
+        timer_delay_ms(100);
+        gpio_write(pin_1, 0);
+        timer_delay_ms(100);
+    }
     while(1);
 }
