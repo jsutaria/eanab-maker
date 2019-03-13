c3caada5ea34303f63b5c4dbc3ab386f18b29179
Jainil Sutaria <jsutaria@stanford.edu>
2019-03-12 21:11:09 -0700

    Fix up breathalyzer

--- src/common/breathalyzer.c
+++ src/common/breathalyzer.c
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-#include "breathalyzer.h"
-#include "malloc.h"
-#include "printf.h"
-#include "strings.h"
 #include "gpio.h"
-
-void breathalyzer_init(unsigned int breathalyzer_pin) {
-  gpio_set_input(breathalyzer_pin);
-}
-unsigned int read_alcohol_level() {
-=======
 #include "beathalyzer.h"
 
 static unsigned int data_gpio;
@@ -17,9 +6,9 @@ static unsigned int data_gpio;
 void breathalyzer_init(unsigned int data_pin)
 {
     data_gpio = data_pin;
+    gpio_set_input(data_pin);
 }
 
 void breathalyzer_read(void) {
->>>>>>> 7215af588dd80397edf44e8b5e7189310a1f45eb
 
 }
