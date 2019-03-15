aa95a8e96d1bcdfd1aec551419c3dbcf349b5aa6
Theo Patt <theo@theopatt.com>
2019-03-15 10:45:39 -0700

    refmt

--- src/maker/main.c
+++ src/maker/main.c
@@ -9,6 +9,21 @@
 #include "communicator.h"
 #include "rand.h"
 
+static storage_ingredients_t * generate_random_mix(void)
+{
+    storage_ingredients_t *ingredients = (storage_ingredients_t *)malloc(4);
+    unsigned int total = 0;
+    for (int i = 0; i < 4; i++) {
+      ingredients[i] = rand() % 255;
+      total += ingredients[i];
+    }
+    for (int i = 0; i < 4; i++) {
+      ingredients[i] = ingredients[i] / total * 255;
+    }
+
+    return ingredients;
+}
+
 void main(void)
 {
     timer_init();
@@ -23,33 +38,16 @@ void main(void)
     while (1) {
         // Swipe ==> start pipeline for drink prep
         magstripe_card_t *card = magstripe_read_next_card();
-
-        // fixme: temporarily set to 123 for testing
-        // char *user = malloc(5);
-        // user = "123";
         char *user = card->track_2->pan;
 
         printf("Looks like %s wants to get a drink!\n", user);
+
         storage_ingredients_t *ingredients;
 
         if (!storage_has_key(user)) {
-            // fixme: flash a red led? some way to indicate error state
-            // printf("Oh no, we couldn't find a saved drink for %s. :(\n", user);
-            // continue;
-            unsigned char *temp = malloc(4);
-            unsigned int total = 0;
-            for (int i = 0; i < 4; i++) {
-              temp[i] = rand() % 255;
-              total += temp[i];
-            }
-            for (int i = 0; i < 4; i++) {
-              temp[i] = temp[i] / total * 255;
-            }
-
-            ingredients = (storage_ingredients_t *)temp;
+            ingredients = generate_random_mix();
         } else {
-          ingredients = (storage_ingredients_t *)storage_get_key(user);
-
+            ingredients = storage_get_key(user);
         }
 
         printf("Nice, we're gonna make %d/%d/%d/%d!\n", ingredients[0], ingredients[1], ingredients[2], ingredients[3]);
