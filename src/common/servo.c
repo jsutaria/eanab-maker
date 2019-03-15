ad94807365d4bb50e457a4243b48503f1682c8ff
Jainil Sutaria <jsutaria@stanford.edu>
2019-03-15 04:13:04 -0700

    Still have no idea how to use pwm... a little help tm maybe @theopolisme @ankushswar1?

--- src/common/servo.c
+++ src/common/servo.c
@@ -4,21 +4,33 @@
 
 void servo_init() {
   pwm_init();
-  pwm_set_clock(19200000);
-  pwm_enable(0);
-  pwm_set_mode(0, 1);
+  gpio_set_function(18, GPIO_FUNC_ALT5);
+  pwm_set_clock(19200000); //from the oscilator
+  pwm_set_mode(0, PWM_MARKSPACE);
   pwm_set_fifo(0, 1);
-  pwm_set_range(0, 4095);
-  pwm_write(100);
+  pwm_enable(0);
+  pwm_set_range(0, 1023);
+
+  pwm_set_width(0, 512);
+  timer_delay(5);
+  
+  pwm_set_width(0, 256);
+  timer_delay(5);
+  //pwm_write(511);
+
+  //timer_delay(3);
+  pwm_write(255);
+  timer_delay(3);
+  pwm_write(127);
   //while(1) printf("%x\n", pwm_get_status());
   //gpio_set_output(GPIO_PIN18);
-  int i = 0;
+//  int i = 0;
 
-  while(1) {
-    gpio_write(GPIO_PIN18, 1);
-    timer_delay_ms(50 - (i % 50));
-    gpio_write(GPIO_PIN18, 0);
-    timer_delay_ms((i % 50));
-  }
+  // while(1) {
+  //   gpio_write(GPIO_PIN18, 1);
+  //   timer_delay_ms(50 - (i % 50));
+  //   gpio_write(GPIO_PIN18, 0);
+  //   timer_delay_ms((i % 50));
+  // }
 
 }
